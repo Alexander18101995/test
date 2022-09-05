20aa057161b9c1ae87a68da521a86b7a610c4242
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-09-05 10:34:27 +0300

    Update test_func.py

--- test_func.py
+++ test_func.py
@@ -5,6 +5,10 @@ class TestFunction(unittest.TestCase):
     def test_people(self):
         result = people('11-2')
         self.assertEqual(result,"Геннадий Покемонов")
+    def test_list(self):
+       for doc in documents:
+       res = list()    
+       self.assertEqual(res, print(doc['type'], doc['number'], doc['name']))
     def test_shelf(self):
         result_2 = shelf('10006')
         self.assertEqual(result_2,'2')
